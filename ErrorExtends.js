5b37760bfe71e9cf17eb2b5a77f04ce9a460d22b
pray-lee <praycis@gmail.com>
2024-09-25 18:51:37 +0800

    udate

---
+++ ErrorExtends.js
@@ -0,0 +1,54 @@
+class ReadError extends Error {
+    constructor(message, cause) {
+        super(message)
+        this.cause = cause
+        this.name = 'ReadError'
+    }
+}
+
+class ValidationError extends Error {}
+class PropertyRequiredError extends ValidationError {}
+
+function validateUser(user) {
+    if(!user.age) {
+        throw new PropertyRequiredError('age')
+    }
+    if(!user.name) {
+        throw new PropertyRequiredError('name')
+    }
+}
+
+function readUser(json) {
+    let user;
+    try {
+        user = JSON.parse(json);
+    }catch(err) {
+        if(err instanceof SyntaxError) {
+            console.log(err,'errr1')
+            throw new ReadError('Syntax Error', err)
+        }
+    }
+    try {
+        validateUser(user)
+    }catch(err) {
+        if(err instanceof ValidationError) {
+            console.log(err, 'errr2')
+            throw new ReadError('validationError', err)
+        }else{
+            throw err
+        }
+    }
+}
+
+// 如果有一个错误，后边的错误就不抛了。
+
+try {
+    // const obj = {name: 'lee'}
+    // readUser(JSON.stringify(obj))
+    readUser('{ssss}') // err1
+}catch(err) {
+    console.log(err, 'err')
+    if(err instanceof ReadError) {
+       console.log('OriginalError: ' + err.cause)
+    }
+}
